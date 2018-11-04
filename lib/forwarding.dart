4a4c8d5c4d1dd255ffe61d29b560899ddcdcc28f
justpianm8 <usangeorg@ya.ru>
2018-11-04 15:34:00 -0400

    Implemented the maing thing

---
+++ lib/forwarding.dart
@@ -0,0 +1,258 @@
+import 'dart:math';
+import 'dart:core';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:sms/sms.dart';
+import 'package:flutter/foundation.dart';
+
+/// Defines forwarder interface.
+abstract class AbstractForwarder {
+  /// Forwards sms to a user
+  Future<bool> forward(SmsMessage sms);
+
+  /// Unnamed constructor
+  AbstractForwarder();
+
+  /// Constructs forwarder from json
+  AbstractForwarder.fromJson(Map json);
+
+  /// Dumps class to json
+  String toJson();
+}
+
+/// A simple forwarder for debugging.
+class StdoutForwarder implements AbstractForwarder {
+  /// Writes received sms to stdout
+  Future<bool> forward(SmsMessage sms) async {
+    print("Received an sms: ${sms.body}.");
+    return Future<bool>(() => true);
+  }
+
+  /// Default constructor
+  StdoutForwarder();
+
+  /// Just to support the interface
+  StdoutForwarder.fromJson(Map json);
+
+  /// Dumps this class to json
+  @override
+  String toJson() => '{"StdoutForwarder": {}}';
+}
+
+/// HTTP forwarder. Provides implementation for the [forward] method and
+/// [mapToUri], but requires user to implement [get].
+abstract class HttpForwarder implements AbstractForwarder {
+  /// Creates uri parameters string out of given [map].
+  /// NOTE: value with key 'thread_id' will be removed.
+  /// Example: `mapToUri({"msg":  "test message", "code": 10})` produces
+  /// `?msg=test%20message&code=10&`
+  static String mapToUri(Map map) {
+    String uri = "?";
+    var body = map
+    // Remove `thread_id`,
+      ..removeWhere((k, v) => k == 'thread_id')
+      // Cast each field to string
+      ..map((k, v) => MapEntry(k, v.toString()));
+    // Encode and build uri parameters
+    body.forEach((k, v) => uri += "$k=${Uri.encodeComponent(v.toString())}&");
+    return uri;
+  }
+
+  /// Makes http request and returns request object.
+  Future<http.Response> get(SmsMessage sms);
+
+  /// Default implementation of [forward].
+  /// Awaits request and returns true if statusCode is 200.
+  @override
+  Future<bool> forward(SmsMessage sms) async {
+    return Future<bool>(() async {
+      // Make GET request
+      var response = await get(sms);
+      // TODO: remove debug prints
+      debugPrint("Response status: ${response.statusCode}.");
+      debugPrint("Response body: \'${response.body}\'.");
+      // Return true if status is 200
+      return response.statusCode == 200;
+    });
+  }
+
+}
+
+/// Forwards SMS to provided [_callbackUrl].
+class HttpCallbackForwarder extends AbstractForwarder with HttpForwarder {
+  /// Messages will be sent to [_callbackUrl]
+  String _callbackUrl;
+
+  /// Getter for the callback
+  String get callbackUrl => _callbackUrl;
+
+  /// Frontend is responsible for checking the {proto}:// part.
+  HttpCallbackForwarder(this._callbackUrl);
+
+  /// Constructs HttpCallbackForwarder from [json]
+  @override
+  HttpCallbackForwarder.fromJson(Map json) {
+    if (json.containsKey("HttpCallbackForwarder")) {
+      json = json["HttpCallbackForwarder"];
+    }
+    _callbackUrl = json["callbackUrl"];
+    // Check if all required fields are provided
+    if (_callbackUrl == null) throw ArgumentError("Bad json");
+  }
+
+  /// Simply converts SMS to a uri,
+  /// then makes a GET request to [_callbackUrl].
+  @override
+  Future<http.Response> get(SmsMessage sms) {
+    String uriParams = HttpForwarder.mapToUri(sms.toMap);
+    return http.get("$_callbackUrl$uriParams");
+  }
+
+  /// Dumps HttpCallbackForwarder to json
+  @override
+  String toJson() {
+    var fields = json.encode({"callbackUrl": _callbackUrl});
+    return '{"HttpCallbackForwarder": $fields}';
+  }
+}
+
+/// Forwards SMS using provided Telegram bot [_token] and user's [_chatId].
+class TelegramBotForwarder extends AbstractForwarder with HttpForwarder {
+  /// Telegram bot token
+  String _token;
+  /// Telegram chat id
+  int _chatId;
+  /// Getter for the token
+  String get token => _token;
+  /// Getter for the chatId
+  int get chatId => _chatId;
+
+  TelegramBotForwarder(this._token, this._chatId);
+
+  /// Constructs TelegramBotForwarder from [json]
+  @override
+  TelegramBotForwarder.fromJson(Map json) {
+    if (json.containsKey("TelegramBotForwarder")) {
+      json = json["TelegramBotForwarder"];
+    }
+    _token = json["token"];
+    _chatId = json["chatId"];
+    // Check if all required fields are provided
+    if (_token == null || _chatId == null) throw ArgumentError("Bad json");
+  }
+
+  /// Constructs base Telegram Bot API url
+  get api {
+    return "https://api.telegram.org/bot$_token";
+  }
+
+  /// Constructs Telegram Bot API url using provided [methodName].
+  String method(String methodName) {
+    return "$api/$methodName";
+  }
+
+  /// Sends SMS data to [_chatId].
+  @override
+  Future<http.Response> get(SmsMessage sms) {
+    // Encode message
+    String uriParams = HttpForwarder.mapToUri({
+      "chat_id": _chatId,
+      "text": "New SMS message from ${sms.address}:\n${sms.body}\n\n"
+              "Date: ${sms.date}."
+    });
+    // Send message to user using Telegram Bot API
+    String url = this.method("sendMessage");
+    return http.get("$url$uriParams");
+  }
+
+  /// Dumps TelegramBotForwarder to json
+  @override
+  String toJson() {
+    var fields = {"token": _token, "chatId": _chatId};
+    return json.encode({"TelegramBotForwarder": fields});
+  }
+}
+
+/// Sends SMS data to a server
+class DeployedTelegramBotForwarder extends HttpCallbackForwarder {
+  String _tgCode;
+  String _baseUrl;
+  String _tgHandle;
+  String _botHandle;
+  bool _isSetUp = false;
+
+  /// Getters
+  bool get isSetUp => _isSetUp;
+  String get baseUrl => _baseUrl;
+  String get tgHandle => _tgHandle;
+  String get botHandle => _botHandle;
+
+  /// Default constructor
+  DeployedTelegramBotForwarder(
+      this._tgHandle, {
+        baseUrl: "https://forwarder.whatever.team",
+        botHandle: "smsforwarderrobot"
+      }) : super("$baseUrl/forward") {
+    _baseUrl = baseUrl;
+    _botHandle = botHandle;
+    _tgCode = _genCode();
+  }
+
+  /// Constructs DeployedTelegramBotForwarder from [json]
+  DeployedTelegramBotForwarder.fromJson(Map json) : super(null) {
+    if (json.containsKey("DeployedTelegramBotForwarder")) {
+        json = json['DeployedTelegramBotForwarder'];
+    }
+    _baseUrl = json['baseUrl'] ?? "https://forward.whatever.team";
+    _botHandle = json['botHandle'] ?? "smsforwarderrobot";
+    _tgHandle = json['tgHandle'];
+    _tgCode = json['tgCode'] ?? _genCode();
+    super._callbackUrl = '$_baseUrl/forward';
+    // Check if all required fields are provided
+    if (_tgHandle == null) throw ArgumentError("Bad json");
+  }
+
+  /// Returns url that updates (or creates) code
+  String getUrl() {
+    return "https://t.me/$_botHandle?start=${_tgCode}_$_tgHandle";
+  }
+
+  /// Checks if user with [_tgHandle] exist on the server
+  Future<bool> checkSetupURL() async {
+    var params = {"username": _tgHandle, "code": _tgCode};
+    var r = await http.get(
+        "$_baseUrl/check_user${HttpForwarder.mapToUri(params)}"
+    );
+    var code = r?.statusCode;
+    _isSetUp = code != null && code == 200;
+    return Future<bool>(() => isSetUp);
+  }
+
+  Future<http.Response> get(SmsMessage sms) {
+    String uriParams = HttpForwarder.mapToUri(sms.toMap);
+    String url = "$_callbackUrl${uriParams}code=$_tgCode&username=$_tgHandle";
+    return http.get(url);
+  }
+
+  /// Generates random 8-character code
+  String _genCode() {
+    var rand = Random();
+    return String.fromCharCodes(
+      new List.generate(8, (_) => rand.nextInt(26) + 65)
+    );
+  }
+
+  /// Dumps DeployedTelegramBotForwarder to json
+  @override
+  String toJson() {
+    var fields = {
+      "tgCode": _tgCode,
+      "baseUrl": _baseUrl,
+      "tgHandle": _tgHandle,
+      "botHandle": _botHandle,
+    };
+    return json.encode({"DeployedTelegramBotForwarder": fields});
+  }
+
+}
---
+++ lib/observer.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sms/sms.dart';
+import 'forwarding.dart';
+
+class ForwarderObserver {
+  // Supported forwarders
+  HttpCallbackForwarder httpCallbackForwarder;
+  TelegramBotForwarder telegramBotForwarder;
+  DeployedTelegramBotForwarder deployedTelegramBotForwarder;
+
+  // Reflections in flutter? WHEN?
+  /// Returns mapping (forwarder name -> forwarding result)
+  Future<Map<String, bool>> forward(SmsMessage sms) async {
+    Map<String, bool> map = {};
+    map["HttpCallbackForwarder"] = await httpCallbackForwarder?.forward(sms);
+    map["TelegramBotForwarder"] = await telegramBotForwarder?.forward(sms);
+    map["DeployedTelegramBotForwarder"] =
+      await deployedTelegramBotForwarder?.forward(sms);
+    return map;
+  }
+
+  /// Returns mapping (forwarder name -> forwarder object)
+  Map<String, AbstractForwarder> asMap() => {
+    "HttpCallbackForwarder": httpCallbackForwarder,
+    "TelegramBotForwarder": telegramBotForwarder,
+    "DeployedTelegramBotForwarder": deployedTelegramBotForwarder,
+  };
+
+  /// Returns list of forwarder objects
+  List<AbstractForwarder> asList() => asMap().values.toList();
+
+  /// Returns mapping (forwarder name -> not null)
+  Map<String, bool> reportReadiness()
+  => asMap().map((k, v) => MapEntry(k, v != null));
+
+  /// Loads forwarders from
+  Future<Map> loadFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    String jsonString = prefs.getString("forwarders") ?? "{}";
+    var map = json.decode(jsonString);
+    httpCallbackForwarder = _tryLoad(() => HttpCallbackForwarder.fromJson(map));
+    telegramBotForwarder = _tryLoad(() => TelegramBotForwarder.fromJson(map));
+    deployedTelegramBotForwarder = _tryLoad(
+            () => DeployedTelegramBotForwarder.fromJson(map));
+
+    print(asList());
+    return Future(() => reportReadiness());
+  }
+
+  /// Attempts to load forwarder using provided closure [fromJson]
+  T _tryLoad<T extends AbstractForwarder>(Function fromJson) {
+    var instance;
+    try {
+      instance = fromJson();
+    } catch (ArgumentError) {}
+    return instance as T;
+  }
+
+  /// Dumps forwarders to shared preferences
+  void dumpToPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    List<String> serialized = [];
+    for (var fwd in asList()) {
+      if (fwd == null) continue;
+      String json = fwd.toJson();
+      // Remove trailing '{' and '}'
+      serialized.add(json.substring(1, json.length - 1));
+    }
+    // Dump serialized forwarders to shared preferences
+    var jsonStr = "{${serialized.join(', ')}}";
+    prefs.setString("forwarders", jsonStr);
+  }
+}
